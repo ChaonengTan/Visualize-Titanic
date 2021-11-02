f9dfdc0d5eee152dc3505bfee9a6389d8e6855bc
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2021-11-01 18:15:26 -0700

    sort by: gender, survived, age

--- script.js
+++ script.js
@@ -14,64 +14,104 @@ titanic.style.gridTemplateColumns = `repeat(${columns}, ${squaresSize+border*2}p
 titanic.style.gridGap = '1px'
 
 // Map over the data and make a new element for each passenger
-const passengers = data.map(p => {
+var passengers = data.map(p => {
   return document.createElement('div')
 })
 
+// clears titanic div
+const clear = () => {
+  titanic.innerHTML = ''
+}
 // Loop over each passenger and append them to the titanic
-passengers.forEach(p => {
-  titanic.appendChild(p)
+const append = () => {
+  passengers.forEach(p => {
+    titanic.appendChild(p)
+  })
+}
+append()
+
+// sort
+document.body.addEventListener('click', e => {
+  if(e.target.className === 'survivedSort') {
+    clear()
+    survivedSort()
+    style()
+    append()
+  }
+  if(e.target.className === 'ageSort') {
+    clear()
+    ageSort()
+    style()
+    append()
+  }
+  if(e.target.className === 'genderSort') {
+    clear()
+    genderSort()
+    style()
+    append()
+  }
 })
+const survivedSort = () => {
+  passengers = data.sort(a => a.fields.survived === 'Yes' ? 1 : -1).map(p => document.createElement('div'))
+}
+const ageSort = () => {
+  passengers = data.sort((a, b) => a.fields.age - b.fields.age).map(p => document.createElement('div'))
+}
+const genderSort = () => {
+  passengers = data.sort(a => a.fields.sex === 'male' ? 1 : -1).map(p => document.createElement('div'))
+}
 
 // styles
-
-// bg color, opacity, border radius, border color
-passengers.forEach((p, i) => {
-  const d = data[i]
-  // BGColor according to embarked
-  p.style.backgroundColor = d.fields.embarked === 'S' ? 'red' : data[i].fields.embarked === 'C' ? 'green' : 'blue'
-  // Opacity according to survived
-  p.style.opacity = d.fields.survived === 'No' ? '.5' : '1'
-  // BorderRadius according to survived
-  p.style.borderRadius = d.fields.sex === 'male' ? '0' : '50%'
-  // BorderColor according to pclass
-  p.style.border = d.fields.pclass && `${border}px solid ${data[i].fields.pclass === 1 ? 'yellow' : data[i].fields.pclass === 2 ? 'gray' : 'brown'}`
-  p.style.width = `${squaresSize}px`
-  p.style.height = `${squaresSize}px`
-})
-// size according to age
-passengers.forEach((p, i) => {
-  const d = data[i]
-  var height = squaresSize
-  var width = squaresSize
-  if (d.fields.age) {
-    switch (Math.floor(d.fields.age/20)) {
-      case(0) :
-        height = height/2
-        width = width/2
-      case(1) :
-        height = height/2
-      case(2) :
-        width = width/2
+const style = () => {
+  // bg color, opacity, border radius, border color
+  passengers.forEach((p, i) => {
+    const d = data[i]
+    // BGColor according to embarked
+    p.style.backgroundColor = d.fields.embarked === 'S' ? 'red' : data[i].fields.embarked === 'C' ? 'green' : 'blue'
+    // Opacity according to survived
+    p.style.opacity = d.fields.survived === 'No' ? '.5' : '1'
+    // BorderRadius according to survived
+    p.style.borderRadius = d.fields.sex === 'male' ? '0' : '50%'
+    // BorderColor according to pclass
+    p.style.border = d.fields.pclass && `${border}px solid ${data[i].fields.pclass === 1 ? 'yellow' : data[i].fields.pclass === 2 ? 'gray' : 'brown'}`
+    p.style.width = `${squaresSize}px`
+    p.style.height = `${squaresSize}px`
+  })
+  // size according to age
+  passengers.forEach((p, i) => {
+    const d = data[i]
+    var height = squaresSize
+    var width = squaresSize
+    if (d.fields.age) {
+      switch (Math.floor(d.fields.age/20)) {
+        case(0) :
+          height = height/2
+          width = width/2
+        case(1) :
+          height = height/2
+        case(2) :
+          width = width/2
+      }
     }
-  }
-  p.style.width = `${width}px`
-  p.style.height = `${height}px`
-  p.style.margin = 'auto'
-})
-// first letter of name
-passengers.forEach((p, i) => {
-  const d = data[i]
-  if (d.fields.name) {
-    const text = document.createElement('div')
-    p.appendChild(text)
-    const name = document.createTextNode(`${d.fields.name[0]}`)
-    text.appendChild(name)
-    const style = text.style
-    style.display = 'flex'
-    style.justifyContent = 'center'
-  }
-})
+    p.style.width = `${width}px`
+    p.style.height = `${height}px`
+    p.style.margin = 'auto'
+  })
+  // first letter of name
+  passengers.forEach((p, i) => {
+    const d = data[i]
+    if (d.fields.name) {
+      const text = document.createElement('div')
+      p.appendChild(text)
+      const name = document.createTextNode(`${d.fields.name[0]}`)
+      text.appendChild(name)
+      const style = text.style
+      style.display = 'flex'
+      style.justifyContent = 'center'
+    }
+  })
+}
+style()
 // Challenges - 
 
 // Make the squares a little bigger 15px by 15px. 
